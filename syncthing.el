ca468011129f9d4120548384a936c0188c85a299
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-06-18 12:18:59 -0600

    Added toggle function to convert buffer files to sync-conflicts and back.

--- syncthing.el
+++ syncthing.el
@@ -61,6 +61,75 @@
      (list (read-file-name "File: " dir nil t nil #'syncthing|conflicting-file-p))))
   (ediff (syncthing|local-file conflicting-file) conflicting-file))
 
+(defun syncthing|current-buffer-file-to-sync-conflict ()
+  (let* ((file-name (buffer-file-name))
+         (new-file-name (concat (file-name-directory file-name)
+                                (file-name-base file-name)
+                                "-sync-conflict."
+                                (file-name-extension file-name))))
+    (rename-buffer new-file-name)
+    (rename-file file-name new-file-name)
+    (set-visited-file-name new-file-name)
+    (set-buffer-modified-p nil)))
+
+(defun syncthing|sync-conflict-file-name (filename)
+  (concat (file-name-directory filename)
+          (file-name-base filename)
+          "-sync-conflict."
+          (file-name-extension filename)))
+
+
+(defun syncthing|file-name-sans-sync-conflict (filename)
+  (concat (file-name-directory filename)
+          (replace-regexp-in-string "-sync-conflict$" "" (file-name-base filename))
+          "."
+          (file-name-extension filename)))
+
+(defun syncthing|rename-current-buffer-file-to-sync-conflict ()
+  "Rename the current buffer and its file so that it register's as a sync conflict with itself."
+  (interactive)
+  (let* ((old-short-name (buffer-name))
+         (old-filename (buffer-file-name))
+         (old-dir (file-name-directory old-filename))
+         (new-name (if (syncthing|conflicting-file-p old-filename)
+                       (syncthing|file-name-sans-sync-conflict old-filename)
+                     (syncthing|sync-conflict-file-name old-filename)))
+         (new-dir (file-name-directory new-name))
+         (new-short-name (file-name-nondirectory new-name))
+         (file-moved-p (not (string-equal new-dir old-dir)))
+         (file-renamed-p (not (string-equal new-short-name old-short-name))))
+    (cond ((get-buffer new-name)
+           (error "A buffer named '%s' already exists!" new-name))
+          (t
+           (let ((old-directory (file-name-directory new-name)))
+             (when (and (not (file-exists-p old-directory))
+                        (yes-or-no-p
+                         (format "Create directory '%s'?" old-directory)))
+               (make-directory old-directory t)))
+           (rename-file old-filename new-name 1)
+           (rename-buffer new-name)
+           (set-visited-file-name new-name)
+           (set-buffer-modified-p nil)
+           (when (fboundp 'recentf-add-file)
+             (recentf-add-file new-name)
+             (recentf-remove-if-non-kept old-filename))
+           (when (and (configuration-layer/package-used-p 'projectile)
+                      (projectile-project-p))
+             (call-interactively #'projectile-invalidate-cache))
+           (message (cond ((and file-moved-p file-renamed-p)
+                           (concat "File Moved & Renamed\n"
+                                   "From: " old-filename "\n"
+                                   "To:   " new-name))
+                          (file-moved-p
+                           (concat "File Moved\n"
+                                   "From: " old-filename "\n"
+                                   "To:   " new-name))
+                          (file-renamed-p
+                           (concat "File Renamed\n"
+                                   "From: " old-short-name "\n"
+                                   "To:   " new-short-name))))))))
+
+
 (provide 'syncthing)
 
 ;;; syncthing.el ends here
